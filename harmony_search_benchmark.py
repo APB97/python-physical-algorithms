b941120f038e9c83c1dd40ebeff99231d0f44f44
APB97 <adrian.bielinski.pl@gmail.com>
2021-04-28 20:33:03 +0200

    Implemented harmony_search_any_func.py and used it in harmony_search_benchmark.py

--- harmony_search_benchmark.py
+++ harmony_search_benchmark.py
@@ -1,14 +1,12 @@
 from math import inf, sqrt
 
-from harmony_search_ackley import search_ackley
-from harmony_search_bukin_n6 import search_bukin_n6
-from harmony_search_rastrigin import search_rastrigin
+from ackley import ackley
+from bukin_n6 import bukin_n6
+from harmony_search_any_func import search
+from rastrigin import rastrigin
 
 
-def benchmark_harmony_search_ackley(repeat_times=20):
-    problem_size = 2
-    problem_bounds = [[-32, 32] for _ in range(problem_size)]
-
+def benchmark_harmony_search(function, bounds, repeat_times=20):
     # algorithm configuration
     mem = 20
     consider = 0.95
@@ -21,7 +19,7 @@ def benchmark_harmony_search_ackley(repeat_times=20):
     best = {'input': None, 'value': inf}
 
     for i in range(0, repeat_times):
-        result = search_ackley(problem_bounds, iters, mem, consider, adjust, harmony_range)
+        result = search(function, bounds, iters, mem, consider, adjust, harmony_range)
         if result['value'] < best['value']:
             best = result
         results.append(result)
@@ -31,76 +29,7 @@ def benchmark_harmony_search_ackley(repeat_times=20):
     stddev = sqrt(sum([(v - mean) ** 2 for v in values]) / (repeat_times - 1))
 
     print("Metoda AI: Poszukiwanie harmonii")
-    print("Funkcja benchmark: Ackley")
-    print(f"Optimum: {best}")
-    print(f"Wyniki ({repeat_times} powtórzeń): ")
-    for result in results:
-        print(result)
-    print(f"Średnia: {mean}")
-    print(f"Odchylenie standardowe: {stddev}")
-
-
-def benchmark_harmony_search_bukin(repeat_times=20):
-    problem_bounds = [[-15, -5], [-3, 3]]
-
-    # algorithm configuration
-    mem = 20
-    consider = 0.95
-    adjust = 0.7
-    harmony_range = 0.05
-    iters = 500
-
-    results = []
-    values = []
-    best = {'input': None, 'value': inf}
-
-    for i in range(0, repeat_times):
-        result = search_bukin_n6(problem_bounds, iters, mem, consider, adjust, harmony_range)
-        if result['value'] < best['value']:
-            best = result
-        results.append(result)
-        values.append(result['value'])
-
-    mean = sum(values) / repeat_times
-    stddev = sqrt(sum([(v - mean) ** 2 for v in values]) / (repeat_times - 1))
-
-    print("Metoda AI: Poszukiwanie harmonii")
-    print("Funkcja benchmark: Bukin no. 6")
-    print(f"Optimum: {best}")
-    print(f"Wyniki ({repeat_times} powtórzeń): ")
-    for result in results:
-        print(result)
-    print(f"Średnia: {mean}")
-    print(f"Odchylenie standardowe: {stddev}")
-
-
-def benchmark_harmony_search_rastrigin(repeat_times=20):
-    problem_size = 2
-    problem_bounds = [[-5.12, 5.12] for _ in range(problem_size)]
-
-    # algorithm configuration
-    mem = 20
-    consider = 0.95
-    adjust = 0.7
-    harmony_range = 0.05
-    iters = 500
-
-    results = []
-    values = []
-    best = {'input': None, 'value': inf}
-
-    for i in range(0, repeat_times):
-        result = search_rastrigin(problem_bounds, iters, mem, consider, adjust, harmony_range)
-        if result['value'] < best['value']:
-            best = result
-        results.append(result)
-        values.append(result['value'])
-
-    mean = sum(values) / repeat_times
-    stddev = sqrt(sum([(v - mean) ** 2 for v in values]) / (repeat_times - 1))
-
-    print("Metoda AI: Poszukiwanie harmonii")
-    print("Funkcja benchmark: Rastrigin")
+    print(f"Funkcja benchmark: {function.__name__}")
     print(f"Optimum: {best}")
     print(f"Wyniki ({repeat_times} powtórzeń): ")
     for result in results:
@@ -110,8 +39,8 @@ def benchmark_harmony_search_rastrigin(repeat_times=20):
 
 
 if __name__ == "__main__":
-    benchmark_harmony_search_ackley()
+    benchmark_harmony_search(ackley, [[-32, 32], [-32, 32]])
     print()
-    benchmark_harmony_search_bukin()
+    benchmark_harmony_search(bukin_n6, [[-15, -5], [-3, 3]])
     print()
-    benchmark_harmony_search_rastrigin()
+    benchmark_harmony_search(rastrigin, [[-5.12, 5.12], [-5.12, 5.12]])
